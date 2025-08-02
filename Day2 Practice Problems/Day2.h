3afb4a796bc8d995f06468c7456e61a3b8687d4e
Shivaprasad Prabhu <prabhushivaprasad17@gmail.com>
2025-08-02 13:42:03 +0530

    Create Day2.h

---
+++ Day2 Practice Problems/Day2.h
@@ -0,0 +1,48 @@
+#define IS_PALINDROME 1
+#define NOT_PALINDROME 0
+#define IS_PRIME 1
+#define NOT_PRIME 0
+#define IS_ARMSTRONG_NUMBER 1
+#define NOT_ARMSTRONG_NUMBER 0
+#define IS_AMICABLE_PAIR 1
+#define NOT_AMICABLE_PAIR 0
+#define CAN_NOT_CALUCULATE 0
+#define SIZE 10
+#define PI 3.1415
+
+#define RATE_TIER1 5
+#define RATE_TIER2 7
+#define RATE_TIER3 10
+#define FIXED_CHARGE1 1000
+#define FIXED_CHARGE2 1700
+
+#define ENTER_POSITIVE_TERMS 0
+
+int sum_of_digits(int);
+int reverse_num(int);
+int occur(int, int);
+int is_Palindrome(int);
+int is_prime(int);
+int gen_prime_number(int);
+int sum_of_series(int);
+int Armstrong_num(int);
+int amicable_pair(int, int);
+int menu_driven_calc(char, int, int);
+
+float calculateCubeVolume(float);
+float calculateCuboidVolume(float, float, float);
+float calculateSphereVolume(float);
+float calculateCylinderVolume(float, float);
+float calculateConeVolume(float, float);
+
+int electricity_bill(int);
+
+int bin_to_dec(int);
+long long dec_to_bin(int);
+
+void sequence(int);
+
+void inverse_half_pyramid(int);
+void half_pyramid(int);
+
+void two_digit_sum_nine(int);
