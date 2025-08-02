cf94326033eb31cf27b558d34c73dcaec6c629b2
Shivaprasad Prabhu <prabhushivaprasad17@gmail.com>
2025-08-02 13:37:29 +0530

    Create Day1.h

---
+++ Day1 Practice Problems/Day1.h
@@ -0,0 +1,14 @@
+#define PI 3.14
+#define TYPE_INT 0
+#define TYPE_CHAR 1
+
+
+char to_lowercase(char ch);
+float CircleArea(float);
+int count(char, char);
+float CtoF(float);
+int is_even(const int);
+int is_leapYear(const int);
+int LeftShiftOperator(int);
+int datatype(char);
+int add(void);
